01969ddd0eb705ebcfaacc3ac86eaf40bd7d8745
morisakikeita01 <32089831+morisakikeita01@users.noreply.github.com>
2022-07-09 00:07:02 +0900

    fixed for production

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,11 @@ let HOST: string = "";
 switch (import.meta.env.MODE) {
   case "development":
     API =  import.meta.env.VITE_APP_DATABASE_URL;
-    HOST = import.meta.env.VITE_APP_BASE_PATH;
+    HOST = import.meta.env.VITE_APP_HOST;
     break;
   case "production":
     API =  import.meta.env.VITE_APP_DATABASE_URL;
-    HOST = import.meta.env.VITE_APP_BASE_PATH;
+    HOST = import.meta.env.VITE_APP_HOST;
     break
   default:
     break;
