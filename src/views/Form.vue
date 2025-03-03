1dd025826d09c873fe926bfced5f3a41e07448c3
kubilayturgut <kubilay.turgut@gmail.com>
2025-03-03 23:59:13 +0900

    2025 date update

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,28 +3,29 @@
     <!-- <VitHeader/> -->
     <div class="template__Wrapper">
       <div class="container relative">
-        <LoadingSpinner v-model="isLoading" />
+        <LoadingSpinner v-model="isLoading"/>
         <div class="relative">
+          <p v-if="room?.header && room?.header !== 'null'" class="sub-title" :style="subTitleStyle">{{ room?.header }}</p>
           <section class="flex-column header-subtext">
             <header class="flex justify-space-between">
               <div class="flex-column title-wrapper">
                 <h1 class="title">{{ pageTitle }}</h1>
                 <span class="timeslot">
-                  {{ arrangeDate}}
+                  {{ arrangeDate }}
                   <span v-if="['746935619','600799837','532783550','520803050'].includes(route.params.rid as string)" class="timeslot-sub">(JST/UTC+9 hours)</span>
                   <span v-if="['604030817', '219272972', '830235141', '304155620'].includes(route.params.rid as string)" class="timeslot-sub">(日本標準時間/UTC＋9小時)</span>
                 </span>
 
               </div>
               <div class="flex justify-center align-center">
-                <div class="back-calendar flex align-center justify-center"  @click="goTo('Room')">
+                <div class="back-calendar flex align-center justify-center" @click="goTo('Room')">
                   <i class="pi pi-calendar"></i>
                   <span class="back-calendar-text">{{ t('formGoToCalendar') }}</span>
                 </div>
               </div>
             </header>
             <p v-if="route.params.rid==='635834411'"
-                class="room-body-summary">
+               class="room-body-summary">
               <span style="color:#e45700;">ご希望の予約時間直前にエントリーされる場合は、出来れば15分程度事前にエントリーいただく事をお勧めします。<br>( ご希望時間直前でのご予約の場合、システムの関係上、ご希望の開始時間を少し遅れる場合がございますので、予めご了承ください。)</span>
             </p>
           </section>
@@ -61,10 +62,11 @@ import ENV from "../config"
 import FormsFunc from "./Forms"
 import useStore from "../helpers/useStore"
 import LoadingSpinner from "@/components/loaders/LoadingSpinner.vue"
-import { DynamicFormRowColumn } from "@/components/dynamic-form/types/DynamicForm";
-import { isRomaji, isKatakana, isMail, isZip, isRomajiWithIrregulars, isNumber, phoneNumberCheck } from "@/components/dynamic-form/helpers/useRules";
+import {DynamicFormRowColumn} from "@/components/dynamic-form/types/DynamicForm";
+import {isRomaji, isKatakana, isMail, isZip, isRomajiWithIrregulars, isNumber, phoneNumberCheck} from "@/components/dynamic-form/helpers/useRules";
 import {useGtm} from "@gtm-support/vue-gtm";
-import { vocabularies } from '../utils/useVocabularies'
+import {vocabularies} from '../utils/useVocabularies'
+import {Room} from "@/types/Room";
 
 interface Extra {
   vacancy?: string,
@@ -86,14 +88,15 @@ export default defineComponent({
     const route = useRoute()
     const router = useRouter();
     const extraData = ref<Extra>({vacancy: "",});
-    const config = ref<Crud>({method: "POST", url: ENV.API+"applicants/"})
+    const config = ref<Crud>({method: "POST", url: ENV.API + "applicants/"})
     const isLoading = ref<boolean>(false)
+    const room = ref<Room | null>(null)
     const time = ref<string>("");
     const date = ref<string>("");
     const modelData = ref<any>();
     let isPageLoaded = false
     const gtm = useGtm();
-    const { t } = vocabularies();
+    const {t} = vocabularies();
     const {
       vacancyID,
       cityOptions,
@@ -106,41 +109,48 @@ export default defineComponent({
       checkVacancy,
       saveSessionData,
     } = FormsFunc()
-
-    const arrangeDate:ComputedRef<string> = computed(() =>{
-      if(t('locale') === 'ja') {
+    const subTitleStyle = computed(() => {
+      return {
+        paddingLeft: "10px",
+        color: Number(room.value?.id) === 880808284 ? "red" : "",
+        border: Number(room.value?.id) === 880808284 ? "1px solid red" : "",
+        marginBottom: "20px",
+      }
+    })
+    const arrangeDate: ComputedRef<string> = computed(() => {
+      if (t('locale') === 'ja') {
         return `${date.value.split('-')[0]} 年
             ${date.value.split('-')[1]}月
             ${date.value.split('-')[2]}日
-            ${time.value.split(':')[0]}:${ time.value.split(':')[1]}`
-      }else if(t('locale') === 'zh') {
+            ${time.value.split(':')[0]}:${time.value.split(':')[1]}`
+      } else if (t('locale') === 'zh') {
         return `${date.value.split('-')[0]} /
             ${date.value.split('-')[1]}/
             ${date.value.split('-')[2]} - 
-            ${time.value.split(':')[0]}:${ time.value.split(':')[1]}`
-      }else{
-        return `${time.value.split(':')[0]}:${ time.value.split(':')[1]},
+            ${time.value.split(':')[0]}:${time.value.split(':')[1]}`
+      } else {
+        return `${time.value.split(':')[0]}:${time.value.split(':')[1]},
             ${date.value.split('-')[1]} /
             ${date.value.split('-')[2]}/
             ${date.value.split('-')[0]}`
       }
     });
 
-    async function init(): Promise<void>{
+    async function init(): Promise<void> {
       // 1. Check if vacancy, if not send back to calendar
       checkVacancy()
-      try{
+      try {
         // 2. Request form data
         const path = ENV.API + "forms/" + formID.value + "/"
         isLoading.value = true
-        const response = await  axios.get<FormItem[]>(path)
+        const response = await axios.get<FormItem[]>(path)
         const data = JSON.parse(JSON.stringify(response.data))
         if (data.title) {
-          pageTitle.value = data.title !== "null" ?  data.title.toString() : ""
-          document.getElementsByTagName('title')[0].innerHTML = (pageTitle.value)?pageTitle.value:"ビターブ｜予約システム作成・予約管理ならおまかせ｜viterve"
+          pageTitle.value = data.title !== "null" ? data.title.toString() : ""
+          document.getElementsByTagName('title')[0].innerHTML = (pageTitle.value) ? pageTitle.value : "ビターブ｜予約システム作成・予約管理ならおまかせ｜viterve"
         }
-        if (data.sub_title){
-          subTitle.value = data.sub_title !== "null" ?  data.sub_title.toString() : ""
+        if (data.sub_title) {
+          subTitle.value = data.sub_title !== "null" ? data.sub_title.toString() : ""
         }
         // 2.5 Setup form data
         setupForm(data.form_rows)
@@ -149,30 +159,33 @@ export default defineComponent({
         loadSession()
 
         // 4. Get vacancy data
-        const response2 =  await axios.get<any>(ENV.API + "vacancies/" + vacancyID.value + "/")
-        if(parseInt(response2.data.applicants.length) < parseInt(response2.data.limit)){
+        const response2 = await axios.get<any>(ENV.API + "vacancies/" + vacancyID.value + "/")
+        if (parseInt(response2.data.applicants.length) < parseInt(response2.data.limit)) {
           date.value = response2.data.date
-          time.value = response2.data.date_time_start.slice( -9, -4 )
+          time.value = response2.data.date_time_start.slice(-9, -4)
           isLoading.value = false
           isPageLoaded = true
           setupExtraData()
-        }else{
+        } else {
           throw t('formLimit')
         }
-      }catch(error){
+      } catch (error) {
         isLoading.value = false;
         store.SET_ERROR({title: "error", text: "An error has occurred."})
         console.log(error)
         goTo("Room")
       }
+
+      getRooms();
+
     }
 
-    function getRows(rows: FormRow[]): DynamicFormRow[]{
+    function getRows(rows: FormRow[]): DynamicFormRow[] {
       const retRows: DynamicFormRow[] = []
-      rows.forEach((row:FormRow, idx: number) => {
+      rows.forEach((row: FormRow, idx: number) => {
         retRows.push(
           {
-            class: (row.column?`column-${row.column}`:"")+" "+row.classes+" "+(row.column==1?'flex justify-space-between':''),
+            class: (row.column ? `column-${row.column}` : "") + " " + row.classes + " " + (row.column == 1 ? 'flex justify-space-between' : ''),
             columns: getColumns(row.form_items),
             margin: "0 0 16px 0",
           }
@@ -181,30 +194,30 @@ export default defineComponent({
       return retRows
     }
 
-    function getColumns(form_items: FormItem[]): DynamicFormRowColumn[]{
-      const retColumns:DynamicFormRowColumn[] = []
-      form_items.forEach((formItem:FormItem) => {
+    function getColumns(form_items: FormItem[]): DynamicFormRowColumn[] {
+      const retColumns: DynamicFormRowColumn[] = []
+      form_items.forEach((formItem: FormItem) => {
         let column: DynamicFormRowColumn =
-        {
-          component: null,
-          columnWidth: formItem.width?""+formItem.width:"",
-          type: formItem.type,
-          model: null,
-          required: formItem.required,
-          disabled: false,
-          name: formItem.title,
-          db: formItem.label,
-          placeholder: ""+formItem.placeholder,
-          class: formItem.column?`column-${formItem.column}-space`:"",
-          props: getColumnProps(formItem),//addOptions if available
-          errors: [],
-          rules: getRulesFunctions(formItem.rules||""),
-          options: getColumnOptions(formItem),
-          optionValue: "value",
-          optionLabel: "label",
-          invalid: false,
-        }
-        if(formItem.hint) {
+          {
+            component: null,
+            columnWidth: formItem.width ? "" + formItem.width : "",
+            type: formItem.type,
+            model: null,
+            required: formItem.required,
+            disabled: false,
+            name: formItem.title,
+            db: formItem.label,
+            placeholder: "" + formItem.placeholder,
+            class: formItem.column ? `column-${formItem.column}-space` : "",
+            props: getColumnProps(formItem),//addOptions if available
+            errors: [],
+            rules: getRulesFunctions(formItem.rules || ""),
+            options: getColumnOptions(formItem),
+            optionValue: "value",
+            optionLabel: "label",
+            invalid: false,
+          }
+        if (formItem.hint) {
           column['hint'] = formItem.hint
         }
         retColumns.push(column)
@@ -212,9 +225,9 @@ export default defineComponent({
       return retColumns
     }
 
-    function getColumnProps(formItem: FormItem): string[]{
+    function getColumnProps(formItem: FormItem): string[] {
       let res = ["placeholder"]
-      if(formItem.options && formItem.options.length>0){
+      if (formItem.options && formItem.options.length > 0) {
         res.push("options")
         res.push("optionValue")
         res.push("optionLabel")
@@ -222,33 +235,33 @@ export default defineComponent({
       return res
     }
 
-    function getColumnOptions(formItem: FormItem): string[]|object[]{
-      if(formItem.type === "select"){
-        if(formItem.title === "都道府県"){
+    function getColumnOptions(formItem: FormItem): string[] | object[] {
+      if (formItem.type === "select") {
+        if (formItem.title === "都道府県") {
           return cityOptions
         }
       }
-      return formItem.options||[]
+      return formItem.options || []
     }
 
-    function getRulesFunctions(rules: string): Function[]{
-      const r:string[] = rules.replace(" ", "").split(",")
+    function getRulesFunctions(rules: string): Function[] {
+      const r: string[] = rules.replace(" ", "").split(",")
       const res: Function[] = []
       // if (rules && rules.length > 0) {
       r.forEach((rule: string) => {
-        if(rule === "katakana"){
+        if (rule === "katakana") {
           res.push(isKatakana)
-        }else if(rule === "email"){
+        } else if (rule === "email") {
           res.push(isMail)
-        }else if(rule === "zipcode"){
+        } else if (rule === "zipcode") {
           res.push(isZip)
-        }else if(rule === "number"){
+        } else if (rule === "number") {
           res.push(isNumber)
-        }else if(rule === "tel"){
+        } else if (rule === "tel") {
           res.push(phoneNumberCheck)
-        }else if(rule === "kana"){
+        } else if (rule === "kana") {
           res.push(isKatakana)
-        }else if(rule === "kankakukigou"){
+        } else if (rule === "kankakukigou") {
           res.push(isRomajiWithIrregulars)
         }
       })
@@ -256,10 +269,10 @@ export default defineComponent({
     }
 
     function setupForm(f: FormRow[]): void {
-      dynForm.value = {class: "",rows: getRows(f)}
+      dynForm.value = {class: "", rows: getRows(f)}
     }
 
-    function setupExtraData(): void{
+    function setupExtraData(): void {
       extraData.value.vacancy = vacancyID.value.toString()
       extraData.value.date = date.value.toString()
       extraData.value.time = time.value.toString()
@@ -283,7 +296,7 @@ export default defineComponent({
       }, 400)
     }
 
-    function loadSession(): void{
+    function loadSession(): void {
       if (hasSessionData()) {
         let d = JSON.parse(getSessionData())
         let f = getSessionFormID()
@@ -305,10 +318,12 @@ export default defineComponent({
               if (column.model) {
                 formData.append(column.db, column.model.toString())
                 //add building name to formData
-                if(column.db==="project" && column.options){
-                  const buildingName:any = [...column.options]
-                    .find((option:any) =>  option.value === column.model?.toString())
-                  if(buildingName) {formData.append("building_name", buildingName.label)}
+                if (column.db === "project" && column.options) {
+                  const buildingName: any = [...column.options]
+                    .find((option: any) => option.value === column.model?.toString())
+                  if (buildingName) {
+                    formData.append("building_name", buildingName.label)
+                  }
                 }
               }
             }
@@ -340,6 +355,23 @@ export default defineComponent({
       })
     }
 
+    function getRooms() {
+      axios.request({
+        method: "get",
+        baseURL: ENV.API,
+        url: "rooms/" + route.params.rid + "/",
+      })
+        .then((response) => {
+
+          const data = JSON.parse(JSON.stringify(response.data))
+          room.value = data
+
+        })
+        .catch((eroor) => {
+          console.log(eroor)
+        })
+    }
+
     const onError = (): void => {
       isLoading.value = false;
       console.error("Server could not accept response")
@@ -348,20 +380,20 @@ export default defineComponent({
     }
 
     const onComplete = (response: any): void => {
-      setTimeout(()=>{
+      setTimeout(() => {
         isLoading.value = false;
-        if(response.data && response.data.status && response.data.status ==="REFUSED"){
+        if (response.data && response.data.status && response.data.status === "REFUSED") {
           store.SET_ERROR({title: "エラー", text: response.data.info}) // text:  //"予約が上限に達しました。他の日時を選択して下さい。"
           goTo('Room')
         }
-        if(response.data && response.data.status && response.data.status ==="OK"){
+        if (response.data && response.data.status && response.data.status === "OK") {
           removeSessionData()
           goTo('Thanks')
         }
-      },600)
+      }, 600)
     }
 
-    const dateAndTime:ComputedRef<string> = computed(() => {
+    const dateAndTime: ComputedRef<string> = computed(() => {
       if (date.value && time.value) {
         let dateN = date.value.split('-')
         let timeN = time.value.split(':')
@@ -375,7 +407,7 @@ export default defineComponent({
     const goTo = (where: string): void => {
       const currentQuery = Object.assign({}, route.query)
       delete currentQuery.vacancy
-      const param = {rid:route.params.rid, fid:route.params.fid}
+      const param = {rid: route.params.rid, fid: route.params.fid}
       if (where === 'Room') {
         param.rid = roomID.value.toString()
       }
@@ -386,23 +418,23 @@ export default defineComponent({
       })
     }
 
-    watch(() => dynForm.value, val =>{
-      if(Object.keys(val).length>0 && isPageLoaded){
+    watch(() => dynForm.value, val => {
+      if (Object.keys(val).length > 0 && isPageLoaded) {
         saveForm()
       }
-    },{deep:true})
+    }, {deep: true})
 
     onMounted(() => {
-      if(!gtm?.enabled()){
+      if (!gtm?.enabled()) {
         gtm?.enable(true)
-      }else{
+      } else {
         window.dataLayer?.push({
           google_tag_params: {
             dynx_pagetype: 'other',
             dynx_device: 'pc'
           }
         });
-        gtm?.trackEvent({event: 'gtm.init_consent', 'content-view-name':route.name});
+        gtm?.trackEvent({event: 'gtm.init_consent', 'content-view-name': route.name});
         gtm?.trackEvent({event: 'gtm.init'});
         gtm?.trackEvent({event: 'gtm.js'});
         gtm?.trackEvent({event: 'gtm.dom'});
@@ -412,9 +444,11 @@ export default defineComponent({
     })
 
     return {
+      room,
+      subTitleStyle,
       date, time, pageTitle, subTitle, dateAndTime,
       dynForm, modelData,
-      extraData,config,
+      extraData, config,
       isLoading, arrangeDate, route,
       submit, onError, onComplete,
       goTo, t,
@@ -429,14 +463,17 @@ export default defineComponent({
   flex-grow: 1;
   padding-top: 66px;
 }
-#request .template__Wrapper{
+
+#request .template__Wrapper {
   flex-grow: 1;
   transition: background-color 0.15s, border-color 0.15s, color 0.15s;
 }
+
 #request section.header-subtext {
   margin-bottom: 30px;
 }
-#request section.header-subtext p.room-body-summary{
+
+#request section.header-subtext p.room-body-summary {
   font-size: 0.75rem;
   margin-top: 20px;
   background-color: #faebd76f;
@@ -448,6 +485,7 @@ header .title-wrapper {
   padding: 3px 0 3px 15px;
   position: relative;
 }
+
 header .title-wrapper::after {
   content: "";
   position: absolute;
@@ -458,6 +496,7 @@ header .title-wrapper::after {
   border-radius: 30px;
   background-color: #6366f1;
 }
+
 header .title-wrapper h1.title {
   font-size: 1.3rem;
   margin-bottom: 0px;
@@ -471,10 +510,12 @@ header .title-wrapper span.timeslot {
   color: #6366f1;
   line-height: 1.1;
 }
+
 header .title-wrapper span.timeslot-sub {
   font-size: 0.7rem;
   padding-left: 3px;
 }
+
 header .back-calendar {
   height: 34px;
   padding: 4px 14px;
@@ -485,9 +526,11 @@ header .back-calendar {
   cursor: pointer;
   color: #6366f1;
 }
+
 header .back-calendar i {
   font-size: 0.9rem;
 }
+
 header span.back-calendar-text {
   display: inline-block;
   position: relative;
@@ -499,8 +542,9 @@ header span.back-calendar-text {
 @media screen and (max-width: 414px) {
   header {
     flex-direction: column;
-    align-items: start;/*center*/
+    align-items: start; /*center*/
   }
+
   header .title-wrapper {
     margin-bottom: 10px;
   }
